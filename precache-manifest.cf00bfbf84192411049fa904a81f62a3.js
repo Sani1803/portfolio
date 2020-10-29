00a9c717c2814064bfaad696b4a14f1769ded305
Sani <aleksandra-gjurova@hotmail.com>
2020-10-29 21:25:26 +0100

    Updates

---
+++ precache-manifest.cf00bfbf84192411049fa904a81f62a3.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d251c5dc72dfe381cd6cd07c243b19b9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2eee5773f553e335da9f",
+    "url": "/portfolio/static/css/main.e1f55cd2.chunk.css"
+  },
+  {
+    "revision": "6deff3e6bc5d0a14dfdc",
+    "url": "/portfolio/static/js/2.6716caf1.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/portfolio/static/js/2.6716caf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eee5773f553e335da9f",
+    "url": "/portfolio/static/js/main.ae5788fb.chunk.js"
+  },
+  {
+    "revision": "452cbb7928d2b162740c",
+    "url": "/portfolio/static/js/runtime-main.9e9558d4.js"
+  },
+  {
+    "revision": "b2342398ce6fa9515ab74434b69822ec",
+    "url": "/portfolio/static/media/BackIMG.b2342398.jpg"
+  },
+  {
+    "revision": "03ab5628adaa8a10bef7a3c871ba6d80",
+    "url": "/portfolio/static/media/CVimg.03ab5628.jpg"
+  },
+  {
+    "revision": "fcf54051497071edbd2a62de9a615f25",
+    "url": "/portfolio/static/media/bootstrap.fcf54051.png"
+  },
+  {
+    "revision": "86e16e18493363074bd8609ec10b4396",
+    "url": "/portfolio/static/media/cardslist.86e16e18.png"
+  },
+  {
+    "revision": "219dd8244453766cc0f632cb2f6b3ae2",
+    "url": "/portfolio/static/media/colorgame.219dd824.png"
+  },
+  {
+    "revision": "5e5600e29cc80088aa5f85c2b9e8d032",
+    "url": "/portfolio/static/media/css.5e5600e2.png"
+  },
+  {
+    "revision": "891320fff0d58a7c161fce6de8cc5e12",
+    "url": "/portfolio/static/media/email.891320ff.png"
+  },
+  {
+    "revision": "13bb93b8e2411fa843ad1f553fdff63d",
+    "url": "/portfolio/static/media/fb.13bb93b8.png"
+  },
+  {
+    "revision": "1d7db4b682671fc7a5773f18c0399d8b",
+    "url": "/portfolio/static/media/github.1d7db4b6.png"
+  },
+  {
+    "revision": "1a24eb30b62192a737edb684d7358f33",
+    "url": "/portfolio/static/media/insta.1a24eb30.png"
+  },
+  {
+    "revision": "5e82a1d65fb35f2f3f4b3255074e7234",
+    "url": "/portfolio/static/media/javascript.5e82a1d6.png"
+  },
+  {
+    "revision": "5ed4ff31cb48a390e13331cb8cd97b08",
+    "url": "/portfolio/static/media/map.5ed4ff31.png"
+  },
+  {
+    "revision": "38b56e9ffafdeb8cf7055bdd78179281",
+    "url": "/portfolio/static/media/name.38b56e9f.png"
+  },
+  {
+    "revision": "dd523d8e39342afd1ebf431ec3be88ed",
+    "url": "/portfolio/static/media/papatap.dd523d8e.png"
+  },
+  {
+    "revision": "98be32403cea37c92fb14b9ac19edf95",
+    "url": "/portfolio/static/media/photoshop.98be3240.png"
+  },
+  {
+    "revision": "701e966ae66f5ad1b916fb1a4610d091",
+    "url": "/portfolio/static/media/react.701e966a.png"
+  },
+  {
+    "revision": "d648bef65aab6e6c8f0ebb8958d55354",
+    "url": "/portfolio/static/media/redux.d648bef6.png"
+  },
+  {
+    "revision": "2adbc5f82addbd40ce64e8f2e2c5b55c",
+    "url": "/portfolio/static/media/todolist.2adbc5f8.png"
+  }
+]);
